5fcc45e8dacdc8fcde7a040d11585a5c9feab88a
TilbeAtav <68746013+TilbeAtav@users.noreply.github.com>
2024-06-21 14:30:24 +0200

    Add files via upload

--- training.py
+++ training.py
@@ -8,12 +8,6 @@ It is important to document your training steps here, including seed,
 number of folds, model, et cetera
 """
 
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.utils import resample
-
-
-
 def train_save_model(cleaned_df, outcome_df):
     """
     Trains a model using the cleaned dataframe and saves the model to a file.
@@ -25,18 +19,18 @@ def train_save_model(cleaned_df, outcome_df):
     
     ## This script contains a bare minimum working example
     random.seed(1) # not useful here because logistic regression deterministic
-
+    
     # Combine cleaned_df and outcome_df
-    model_df = pd.merge(df, outcome_df, on="nomem_encr")
+    model_df = pd.merge(cleaned_df, outcome_df, on="nomem_encr")
 
     # Filter cases for whom the outcome is not available
     model_df = model_df[~model_df['new_child'].isna()]  
     
     # Logistic regression model
-    model = LogisticRegression(max_iter=500)
+    model = LogisticRegression()
 
     # Fit the model
-    model.fit(model_df[['age']], model_df['new_child'])
+    model.fit(model_df[['cf20m003', 'cf20m004', 'cf20m128', 'ci20m379']], model_df['new_child'])
 
     # Save the model
     joblib.dump(model, "model.joblib")
