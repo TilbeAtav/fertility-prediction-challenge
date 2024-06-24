ac0cde31d67bf50e137438ae74334945df8287d3
TilbeAtav <68746013+TilbeAtav@users.noreply.github.com>
2024-06-24 15:23:23 +0200

    Add files via upload

--- training.py
+++ training.py
@@ -19,15 +19,62 @@ def train_save_model(cleaned_df, outcome_df):
     
     # Combine cleaned_df and outcome_df
     model_df = pd.merge(cleaned_df, outcome_df, on="nomem_encr")
+    
+    features = [
+        "cf20m003",
+        "cf20m128", 
+        "cf20m004",  
+        "ci20m379",
+        "cf20m013",
+        "cf20m014",
+        "cf20m015",
+        "cf20m016",
+        "cf20m020",
+        "cf20m022",
+        "cf20m024",
+        "cf20m025",
+        "cf20m027",
+        "burgstat_2020",
+        "woonvorm_2020",
+        "oplmet_2020"
+    ] 
+    
 
     # Filter cases for whom the outcome is not available
     model_df = model_df[~model_df['new_child'].isna()]  
     
-    # Logistic regression model
-    model = LogisticRegression()
+    # Upsampling only done for training sample, then applied to true test sample
+    oversamp_df = resample(model_df[model_df["new_child"] == 1],
+                        replace=True,
+                        n_samples=model_df[model_df["new_child"] == 0].shape[0],
+                        random_state=123)
+
+    model_df = pd.concat((model_df[model_df["new_child"] == 0], oversamp_df))
+    
+    
+    
+    # Impute missing 
+    imputer = KNNImputer(n_neighbors=2, weights="uniform").set_output(transform = "pandas")
+    imputer.fit_transform(model_df[features])
+    
+    
+    # Transform data
+    numerical_columns = features
+    #categorical_columns = [""]
+    
+    categorical_preprocessor = OneHotEncoder(handle_unknown="ignore")
+    numerical_preprocessor = StandardScaler()
+    
+    preprocessor = ColumnTransformer([
+    ('one-hot-encoder', categorical_preprocessor, categorical_columns),
+    ('standard_scaler', numerical_preprocessor, numerical_columns)])
+    
+
+    # Model
+    model = make_pipeline(imputer, preprocessor, LogisticRegression())
 
     # Fit the model
-    model.fit(model_df[["cf20m003", "cf20m128", "cf20m004", "ci20m379"]], model_df["new_child"])
+    model.fit(model_df[features], model_df["new_child"])
 
     # Save the model
     joblib.dump(model, "model.joblib")
